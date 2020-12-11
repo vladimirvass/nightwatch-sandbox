b5a93e7fb6fd807d7408fb5609ee7a21d79f6d3f
vladimirvass <47001757+vladimirvass@users.noreply.github.com>
2020-12-11 11:12:59 +0200

    Adding travis configuration

--- tests/firstTest.js
+++ tests/firstTest.js
@@ -2,6 +2,8 @@ module.exports = {
     'First experiment test case'(browser) {
         browser
             .url('https://www.yahoo.com/')
+            .waitForElementVisible('.con-wizard')
+            .click('button.primary')
             .waitForElementVisible("[name='p']")
             .assert.visible("[name='p']");
     }
