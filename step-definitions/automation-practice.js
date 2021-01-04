8f5fb5a343b2d6e164cc59a787a08abfcf7946e4
vladimirvass <47001757+vladimirvass@users.noreply.github.com>
2021-01-04 18:09:06 +0200

    FIAT-fix it again Tonny

--- step-definitions/automation-practice.js
+++ step-definitions/automation-practice.js
@@ -2,7 +2,7 @@ const { client } = require('nightwatch-api');
 const { Given, Then, When } = require('cucumber');
 
 Given(/^I open Automation practice's search page$/, () => {
-  return client.url('http://automationpractice.com').waitForElementVisible('body', 1000);
+  return client.url('http://'+'automationpractice.com').waitForElementVisible('body', 1000);
 });
 
 Then(/^the title is "([^"]*)"$/, title => {
